cf59542a3e45826efcdbefd69162e895abbd1be1
ByancaMatos01 <122841376+ByancaMatos01@users.noreply.github.com>
2023-03-06 09:56:32 -0300

    thread

--- src/view/Main.java
+++ src/view/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		for (int idThread=0; idThread<5; idThread++) {
 			ThreadID threadID = new ThreadID(idThread);
+			threadID.start();
 		}
 
 	}
